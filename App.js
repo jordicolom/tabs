325d4cb79d76eb1954ff3b109f5df3c224dc39eb
jordi <trabinat@stcnedap.com>
2021-07-27 11:35:29 +0200

    first

---
+++ App.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/FontAwesome';
+import HomeScreen from './src/screens/HomeScreen';
+import ChatScreen from './src/screens/ChatScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Chat") {
+              iconName = "folder";
+            } else if (route.name === "Settings") {
+              iconName = "book";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Chat" component={ChatScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer >
+  );
+}
---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class ChatScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Chat</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default ChatScreen;
